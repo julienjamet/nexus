770ccc7046f22ad01aac3a637f8c8b5c72f19f5f
julienjamet <j.jamet@alphalink.fr>
2024-10-20 22:21:24 +0200

    backend serves built frontend

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,9 @@
 /****************************************************************IMPORTS*/
 /****************************************NPM MODULES*/
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import path from 'path';
 /****************************************************/
 
 /***************************************NEXUS ROUTER*/
@@ -26,6 +27,21 @@ app.use(express.json());
 
 /***************************************NEXUS ROUTER*/
 NexusRouter(app);
+/****************************************************/
+
+/**********************REDIRECTION TO BUILD FRONTEND*/
+const frontDirPath: string = path.resolve(__dirname, '../../frontend/build/');
+const frontIndexPath: string = path.resolve(frontDirPath, 'index.html');
+
+app.use(express.static(path.join(frontDirPath)));
+
+app.get('/*', (req: Request, res: Response): void => {
+    res.sendFile(frontIndexPath, (error: Error): void => {
+        if (error) {
+            res.status(500).send(error);
+        }
+    });
+});
 /****************************************************/
 
 /******************************************LISTENING*/
